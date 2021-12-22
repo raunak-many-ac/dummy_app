a72ace71c6260846fa86aaac7a414947fad470f6
Raunak Narayan <raunak.smvit@gmail.com>
2021-12-22 21:36:29 +0530

    changing names

--- lib/lottie_sliders/Constants.dart
+++ lib/lottie_sliders/Constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-class JourneyThemeConstants {
+class Constants {
   static const String Screen1 = "Screen1";
   static const String Screen2 = "Screen2";
   static const String Screen3 = "Screen3";
